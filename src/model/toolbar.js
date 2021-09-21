18d8674f444cfe2d6dfd2a414b42af2394aef7c2
thoughtsunificator <85041649+thoughtsunificator@users.noreply.github.com>
2021-09-21 19:18:47 +0200

    clean up

--- src/model/toolbar.js
+++ src/model/toolbar.js
@@ -1,5 +1,4 @@
 export default {
 	tagName: "div",
-	className: "teniireta-toolbar padding-2xs",
-	style: "display: flex; flex-wrap: wrap; background-color: #4064b8; position: relative"
+	className: "teniireta-toolbar"
 }
